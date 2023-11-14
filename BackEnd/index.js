6b713426cd480e7ab48a4ccd63008f651dbfae60
MATEUS SANTANA DE OLIVEIRA <mateus_oliveira8@estudante.sesisenai.org.br>
2023-11-14 18:15:45 -0300

    alterações no backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,6 +2,7 @@ import  Express  from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import Connection from "./database.js";
+import User from "./models/User.js"
 
 
 const server = Express();
@@ -13,6 +14,19 @@ try {
 } catch (error) {
     console.error(error)
 }
+
+const newUser =  new User({
+    name: 'Eduardo',
+    email: 'eduardo@gmail.com',
+    senha: '123456'
+})
+
+const salvar = async () => {
+    const savedUser = await newUser.save()
+    console.log(savedUser)
+}
+
+salvar()
 
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: true}))
